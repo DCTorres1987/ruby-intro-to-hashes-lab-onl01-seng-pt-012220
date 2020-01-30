3bfa22422088556ca6f2956b0bb435f4fe344b96
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-30 13:09:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	    :railroads => 
 	    {:pieces=> 4,
 	     :names => {:reading_railroad=>{"mortgage_value"=>"$100"},
-	                :pennsylvania_railroad=>{"mortgage_value"=>"$200"},
+	                :pennsylvania_railroad=>{"mortgage_value=>"$200"},
 	                :b_and_o_railroad=>{},
 	                :shortline=>{}
 	     },
